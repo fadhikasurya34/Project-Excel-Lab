2a50b8cbdeb00d25ff9ba967f84f4e50d3c90fd9
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 14:15:15 +0700

    update index

--- api/index.php
+++ api/index.php
@@ -1,2 +1,23 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// 1. Load Autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Boot Application
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// 3. Handle Request Secara Eksplisit
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+// 4. Kirim Output ke Browser
+$response->send();
+
+$kernel->terminate($request, $response);
