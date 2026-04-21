b3e1d40293c0b37ca620978395e987aad1ac43cb
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 09:09:16 +0700

    Fix: stable entry point in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,23 @@
 <?php
 
-// Menggunakan realpath untuk memastikan folder ditemukan di server Vercel
-require __DIR__ . '/../public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// 1. Cek Mode Maintenance
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// 2. Registrasi Autoloader (Pintu utama vendor)
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+}
+
+// 3. Bootstrap Laravel
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// 4. Handle Request
+$app->handleRequest(Request::capture());
