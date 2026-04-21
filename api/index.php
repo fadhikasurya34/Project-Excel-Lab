86f9b5f558a87b9474d99d6dc9b78d908b78cc2b
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 11:39:00 +0700

    Final: Migrasi database ke TiDB dan konfigurasi SSL

--- api/index.php
+++ api/index.php
@@ -1,28 +1,3 @@
 <?php
 
-// Mengarahkan folder storage ke /tmp agar Laravel bisa menulis cache/views
-$storagePath = '/tmp/storage/framework';
-foreach (['/sessions', '/views', '/cache'] as $path) {
-    if (!is_dir($storagePath . $path)) {
-        mkdir($storagePath . $path, 0755, true);
-    }
-}
-
-// Paksa Laravel menggunakan folder tmp untuk kompilasi view
-putenv("VIEW_COMPILED_PATH=/tmp/storage/framework/views");
-
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require __DIR__.'/../vendor/autoload.php';
-
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
+require __DIR__ . '/../public/index.php';
