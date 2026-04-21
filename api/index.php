daccfc44389e399d22b81b105aeb8db392e7217d
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 13:31:35 +0700

    Fix: final sync for api index, vercel ignore, and config

--- api/index.php
+++ api/index.php
@@ -1,14 +1,2 @@
 <?php
-
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// 1. Muat Autoloader (Wajib paling atas)
-require __DIR__ . '/../vendor/autoload.php';
-
-// 2. Muat Bootstrap Laravel
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// 3. Jalankan Aplikasi
-$app->handleRequest(Request::capture());
+require __DIR__ . '/../public/index.php';
