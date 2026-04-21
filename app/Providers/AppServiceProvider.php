c71472548fe2e5a30df59d5268adc4088684bc24
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 18:29:08 +0700

    update appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,16 +9,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Jika berjalan di Vercel
+        if (env('VERCEL_URL')) {
+            $this->app->bind('path.public', function () {
+                return base_path('public');
+            });
+        }
     }
 }
