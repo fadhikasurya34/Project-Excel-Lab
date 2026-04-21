0cf83a243af4708c3bafdee8895116a8c2c61956
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 12:44:38 +0700

    Fix: beneran hapus cache yang nyangkut di github

--- api/index.php
+++ api/index.php
@@ -4,19 +4,30 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. MUAT AUTOLOADER (Ini wajib nomor satu!)
+// 1. Muat Autoload
 require __DIR__.'/../vendor/autoload.php';
 
-// 2. JALANKAN BOOTSTRAP LARAVEL
+// 2. Jalankan Bootstrap
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// 3. PAKSA STORAGE KE /TMP (Khusus Vercel)
+// 3. PAKSA jalur storage ke /tmp (Satu-satunya folder yang bisa ditulis di Vercel)
 $app->useStoragePath('/tmp');
 
-// 4. PASTIKAN FOLDER VIEWS ADA (Biar nggak Error 500)
-if (!is_dir('/tmp/framework/views')) {
-    mkdir('/tmp/framework/views', 0755, true);
+// 4. Pastikan folder pendukung ada di /tmp
+$storageFolders = [
+    '/tmp/framework/views',
+    '/tmp/framework/sessions',
+    '/tmp/framework/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0755, true);
+    }
 }
 
-// 5. TANGANI REQUEST
+// 5. Paksa Laravel menggunakan folder view yang baru dibuat
+config(['view.compiled' => '/tmp/framework/views']);
+
+// 6. Jalankan Aplikasi
 $app->handleRequest(Request::capture());
