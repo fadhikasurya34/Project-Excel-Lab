5f264118a9e659711c70918d1d26201cc04262c4
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 12:35:01 +0700

    Fix: forcing storage and view path to tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -5,16 +5,17 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. Cek mode maintenance
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// 2. Register Autoloader (Vercel akan menginstal ini otomatis)
-require __DIR__.'/../vendor/autoload.php';
-
-// 3. Jalankan Bootstrap Laravel
+// 1. Paksa Laravel buat pake folder /tmp (Satu-satunya tempat yang boleh ditulis di Vercel)
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// 4. Tangani Request
+$app->useStoragePath('/tmp');
+
+// 2. Pastikan folder cache view ada di /tmp agar tidak error "view does not exist"
+if (!is_dir('/tmp/framework/views')) {
+    mkdir('/tmp/framework/views', 0755, true);
+}
+
+// 3. Jalankan Autoload & Handle Request
+require __DIR__.'/../vendor/autoload.php';
+
 $app->handleRequest(Request::capture());
