61f29862346b84eb7cfe7b52072dc738fd8503d7
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 11:51:17 +0700

    Fix: menggunakan direct entry di api/index.php untuk bypass path error

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// 1. Cek mode maintenance
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// 2. Register Autoloader (Vercel akan menginstal ini otomatis)
+require __DIR__.'/../vendor/autoload.php';
+
+// 3. Jalankan Bootstrap Laravel
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// 4. Tangani Request
+$app->handleRequest(Request::capture());
