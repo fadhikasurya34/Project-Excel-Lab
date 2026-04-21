a8fb27d0660c088881f5be7c7a5e32d74908bb17
fadhikasurya34 <fadhikasurya34@gmail.com>
2026-04-21 09:34:54 +0700

    Fix: move storage framework to tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,23 +1,28 @@
 <?php
 
+// Mengarahkan folder storage ke /tmp agar Laravel bisa menulis cache/views
+$storagePath = '/tmp/storage/framework';
+foreach (['/sessions', '/views', '/cache'] as $path) {
+    if (!is_dir($storagePath . $path)) {
+        mkdir($storagePath . $path, 0755, true);
+    }
+}
+
+// Paksa Laravel menggunakan folder tmp untuk kompilasi view
+putenv("VIEW_COMPILED_PATH=/tmp/storage/framework/views");
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. Cek Mode Maintenance
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// 2. Registrasi Autoloader (Pintu utama vendor)
-if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-    require __DIR__.'/../vendor/autoload.php';
-}
+require __DIR__.'/../vendor/autoload.php';
 
-// 3. Bootstrap Laravel
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// 4. Handle Request
 $app->handleRequest(Request::capture());
